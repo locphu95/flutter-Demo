3796a3184f7866a4caebc184a39e02bd2178fd85
locphu95 <locphu95@gmail.com>
2020-02-09 13:48:31 +0700

    int

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){ runApp(
+  new Container(
+    decoration: new BoxDecoration( color : Colors.yellowAccent),
+    child:  new Center(
+      child: new Text("Hello new App"),
+    ),
+  )
+);
+}
